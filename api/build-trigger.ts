22922a867a988631cb51cfa269b85431aec69076
mayfly <59794462+x-glorious@users.noreply.github.com>
2023-12-08 17:53:55 +0800

    fix: use custom install command

--- api/build-trigger.ts
+++ api/build-trigger.ts
@@ -1,8 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World111' } = req.query
   return res.json({
-    message: `Hello ${name}!`,
+    message: `Hello world`,
   })
 }
